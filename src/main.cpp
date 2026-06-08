59bc48945654c0dce1aa739d930eb3819a5757f3
shuvaaashish <gyawalishuva2007@gmail.com>
2026-06-08 21:12:12 +0545

    Refactor platformio.ini and add serial control functionality in serial.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
 
-#define LED 2  
+#define LED 2
 
-void setup() {
+void setup(){
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
+
 }
 
-void loop() {
-  digitalWrite(LED, HIGH);
-  Serial.println("LED ON");
+void loop(){
+  digitalWrite(LED,HIGH);
+  Serial.println("ON");
   delay(1000);
-
-  digitalWrite(LED, LOW);
-  Serial.println("LED OFF");
+  
+  digitalWrite(LED,LOW);
+  Serial.println("OFF");
   delay(1000);
 }
---
+++ src/serial.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+
+#define LED 2 
+void setup() {
+  Serial.begin(115200);
+  delay(1000); 
+  pinMode(LED, OUTPUT);
+  Serial.println("Type 'ON' to light up the blue LED.");
+  Serial.println("Type 'OFF' to turn it off.");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String incomingText = Serial.readStringUntil('\n');
+    incomingText.trim(); 
+    Serial.print("ESP32 Received: ");
+    Serial.println(incomingText);
+    if (incomingText == "ON" || incomingText == "on") {
+      digitalWrite(LED, HIGH);
+      Serial.println(">> Success: Turning Blue LED ON!");
+    } 
+    else if (incomingText == "OFF" || incomingText == "off") {
+      digitalWrite(LED, LOW);
+      Serial.println(">> Success: Turning Blue LED OFF!");
+    } 
+    else {
+      Serial.println(">> Error: Unknown command. Try typing 'ON' or 'OFF'.");
+    }
+  }
+}
