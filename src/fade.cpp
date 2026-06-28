eac7f27f0208dee4d051fc21369cf319aa006e58
Shuva Aashish Gyawali <gyawalishuva2007@gmail.com>
2026-06-28 11:43:37 +0545

    Update fade.cpp

--- src/fade.cpp
+++ src/fade.cpp
@@ -1,17 +1,22 @@
 #include <Arduino.h>
 
+#define LED_PIN 2
+#define PWM_CHANNEL 0
+#define PWM_FREQUENCY 5000
+#define PWM_RESOLUTION 8
+
 void setup() {
-  ledcSetup(0, 5000, 8); // channel 0, 5kHz, 8-bit
-  ledcAttachPin(2, 0);   // GPIO2 = built-in LED
+  ledcSetup(PWM_CHANNEL, PWM_FREQUENCY, PWM_RESOLUTION); // channel 0, 5kHz, 8-bit
+  ledcAttachPin(LED_PIN, PWM_CHANNEL);   // GPIO2 = built-in LED
 }
 
 void loop() {
   for (int i = 0; i <= 255; i++) {
-    ledcWrite(0, i);
+    ledcWrite(PWM_CHANNEL, i);
     delay(10);
   }
   for (int i = 255; i >= 0; i--) {
-    ledcWrite(0, i);
+    ledcWrite(PWM_CHANNEL, i);
     delay(10);
   }
 }
